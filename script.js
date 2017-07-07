07b025f46dc147ac9d23979d7ad7a1c29d27b8fc
Marcus Cochran <mcochran@westinghousehs.org>
2017-07-07 19:28:14 +0000

    commit message

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // JavaScript File
 // JavaScript File
-$( document ).ready(function() {
-
-
+$("button").click(function() {
+var answer = "No";
+var input = input;
+if(answer === input){
+    $("#answer").text("Correct!");
+    $("#answer").css("color", "green");
+}else {
+    $("#answer").text("Sorry, try again");
+    $("#answer").css("color", "red");
+}
 });
